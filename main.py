242c4e5646dafeccd2d8078c560d51c3ec15c93a
Adroit10 <tripathidevansh7@gmail.com>
2025-05-18 16:38:30 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from src.DiseaseClassifier import logger
-logger.info("Welcome to custom logging")
+from src.DiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx================x")
+except Exception as e:
+    raise e

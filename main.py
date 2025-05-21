968ed6c57e446f9dd638d7d999fe99121a05db68
Adroit10 <tripathidevansh7@gmail.com>
2025-05-21 23:11:36 +0530

    model training pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.DiseaseClassifier import logger
 from src.DiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.DiseaseClassifier.pipeline.stage_02_base_model_prep import PrepareBaseModelTrainingPipeline
+from src.DiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
@@ -15,6 +16,16 @@ try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
     base_model = PrepareBaseModelTrainingPipeline()
     base_model.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========================x")
+except Exception as e:
+    raise e
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"**********************")
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========================x")
 except Exception as e:
     raise e
